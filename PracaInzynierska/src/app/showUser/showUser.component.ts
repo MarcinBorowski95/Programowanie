55588660da9379efa142221b1ddfad1b09b128b5
Marcin <bomarc2@gmail.com>
2017-12-22 19:50:59 +0100

    ShowUserV1

--- PracaInzynierska/src/app/showUser/showUser.component.ts
+++ PracaInzynierska/src/app/showUser/showUser.component.ts
@@ -1,15 +1,27 @@
+import { DatabaseService } from './../_services/Database.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-showUser',
   templateUrl: './showUser.component.html',
-  styleUrls: ['./showUser.component.css']
+  styleUrls: ['./showUser.component.css'],
+  providers: [DatabaseService]
 })
 export class ShowUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dbService: DatabaseService
+  ) { }
+
+  users;
+  
 
   ngOnInit() {
+    this.users = this.dbService.getUsers();
   }
 
+  changeFlag(user)
+  {
+    
+  }
 }
